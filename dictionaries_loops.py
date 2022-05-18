070291c2152950b3e43804efe6eaf6c53078684c
Bato Tsybenov <taskbato@gmail.com>
2022-05-18 10:56:41 -0400

    mdr

--- dictionaries_loops.py
+++ dictionaries_loops.py
@@ -44,7 +44,7 @@ print(friends)
 favorite_languages = {'jen': 'python', 'sarah': 'c', 'edward': 'ruby', 'phil': 'python'}
 for name in sorted(favorite_languages.keys()):
     print(f"{name}'s favorite language is {favorite_languages[name]}")
-
+#fdsvdvblkdfvslkdcvnslkcm
 print(" **********  exercise 6-5 *******************")
 """6-5. Rivers: Make a dictionary containing three major rivers and the country
 each river runs through. One key-value pair might be 'nile': 'egypt'.
